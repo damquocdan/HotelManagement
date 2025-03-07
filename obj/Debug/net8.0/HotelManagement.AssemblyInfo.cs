135e2085eb4e9286226d46f6ff474c1ce3e21832
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2025-03-07 08:16:53 +0700

    07/03

--- obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+581e07c6e45e97f94057bbdb9f94c94330841de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
